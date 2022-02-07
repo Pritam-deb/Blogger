1edc89a509b3f0c150474270b44f7a3042046ec9
Pritam Debnath <49619791+Pritam-deb@users.noreply.github.com>
2022-02-07 16:25:22 +0530

    feat: connected to MongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,17 @@
 
 import express from "express"
+import dotenv from "dotenv"
+import mongoose from "mongoose"
+
+
+
+
+dotenv.config()
 const app = express()
 const PORT = 5000
+
+
+mongoose.connect(process.env.MONGO_URL, ()=>console.log("Conntected to mongoDB"));
 
 app.use('/', (req, res) => {
   console.log("Backend is running!");

77638bf6a6cb8d73291151519b2d36bb648efad2
Pritam Debnath <49619791+Pritam-deb@users.noreply.github.com>
2022-02-07 20:21:20 +0530

    feat: delete user and read user

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -8,6 +8,7 @@ dotenv.config()
 
 //Model
 import User from "../models/User.js";
+import Post from "../models/Post.js"
 
 const router = Router();
 
@@ -32,6 +33,35 @@ router.put('/:id', async (req, res) => {
 })
 
 //delete user
+router.delete('/:id', async (req, res) => {
+    if (req.body.userId === req.params.id) {
+        try {
+            const user = await User.findById(req.params.id);
+            try {
+                await Post.deleteMany({username: user.username})
+                await User.findByIdAndDelete(req.params.id)
+                res.status(201).send("The user has been deleted");
+            } catch (err) {
+                res.status(500).json(err)
+            }
+        } catch (error) {
+            res.status(404).send("User not found")
+        }
+    } else {
+        res.status(401).send("Please only delete your own account")
+    }
+})
+
+//show user
+router.get('/:id', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        const { password, ...others } = user._doc;
+        res.status(200).send(others)
+    } catch (err) {
+        res.status(400).send(err)
+    }
+})
 
 
 export default router;

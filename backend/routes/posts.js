f7cf7f09e6d40010cf2e9b2b215f68696defc62c
Pritam Debnath <49619791+Pritam-deb@users.noreply.github.com>
2022-02-07 20:36:54 +0530

    feat: update post

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -26,21 +26,26 @@ router.post('/', async (req, res) => {
 
 //update post
 router.put('/:id', async (req, res) => {
-    if (req.body.userId === req.params.id) {
-        if (req.body.password) {
-            const salt = await bcrypt.genSalt(10);
-            req.body.password = await bcrypt.hash(req.body.password, salt);
-        }
-        try {
-            const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (post.username === req.body.username) {
+          try {
+            const updatedPost = await Post.findByIdAndUpdate(
+              req.params.id,
+              {
                 $set: req.body,
-            }, { new: true });
-            res.status(201).send(updatedUser);
-        } catch (err) {
-            res.status(500).json(err)
+              },
+              { new: true }
+            );
+            res.status(200).json(updatedPost);
+          } catch (err) {
+            res.status(500).json(err);
+          }
+        } else {
+          res.status(401).json("You can update only your post!");
         }
-    } else {
-        res.status(401).send("Please only update your own account")
+      } catch (err) {
+        res.status(500).json(err);
     }
 })
 

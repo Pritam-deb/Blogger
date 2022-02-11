0ab429603f2b48f99d56f9f7c8c3fe9b09c57a4e
Pritam Debnath <49619791+Pritam-deb@users.noreply.github.com>
2022-02-11 20:23:44 +0530

    feat: accessing user with contextAPI

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -7,7 +7,7 @@ import axios from "axios";
 const Login = () => {
   const userRef = useRef();
   const passwordRef = useRef();
-  const { user, dispatch, isFetching } = useContext(Context);
+  const { dispatch, isFetching } = useContext(Context);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -22,7 +22,6 @@ const Login = () => {
       dispatch({ type: "LOGIN_FAILURE" });
     }
   };
-  console.log(user);
   return (
     <div className="login">
       <span className="loginTitle">Login</span>

4f652bb16afd498073803ac7fa2265c3f8eca74e
Pritam Debnath <49619791+Pritam-deb@users.noreply.github.com>
2022-02-11 11:13:52 +0530

    feat: added header to homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,18 @@
-
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 //components and pages
 import NavBar from './components/NavBar';
+import Homepage from "./pages/Home/Homepage";
 
 function App() {
   return (
-      <NavBar/>
+    <div>
+    
+    <NavBar/>
+    <Homepage/>
+    </div>
+      
+    
   );
 }
 
---
+++ frontend/src/pages/Home/Homepage.jsx
@@ -0,0 +1,13 @@
+// import { useLocation } from "react-router";
+import Header from "../../components/Header";
+import "./homepage.css";
+
+export default function Homepage() {
+  // const location = useLocation();
+  // console.log(location);
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}

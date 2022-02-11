f5869ccdee12d9b3ee9c77d7e5d587143429933d
Pritam Debnath <49619791+Pritam-deb@users.noreply.github.com>
2022-02-11 19:22:31 +0530

    feat: register user from FE into DB

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,37 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 //components and pages
-import NavBar from './components/NavBar';
+import NavBar from "./components/NavBar";
 import Homepage from "./pages/Home/Homepage";
-import Login from './pages/login/Login';
-import Register from './pages/register/Register';
-import Write from './pages/write/Write'
-import Settings from './pages/settings/Settings'
-import Single from './pages/singlePost/Single'
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import Write from "./pages/write/Write";
+import Settings from "./pages/settings/Settings";
+import Single from "./pages/singlePost/Single";
 
 function App() {
   const currentUser = false;
   return (
     <Router>
-      <NavBar/>
+      <NavBar />
       <Routes>
-        <Route exact path="/" element={<Homepage/>}/>          
-        <Route path="/posts" element={<Homepage/>} />
-        <Route path="/register" element={currentUser ? <Homepage /> : <Register />} />
-        <Route path="/login" element={currentUser ? <Homepage /> : <Login />} />{}
+        <Route exact path="/" element={<Homepage />} />
+        <Route path="/posts" element={<Homepage />} />
+        <Route
+          path="/register"
+          element={currentUser ? <Homepage /> : <Register />}
+        />
+        <Route path="/login" element={currentUser ? <Homepage /> : <Login />} />
+        {}
         <Route path="/post/:id" element={<Single />} />
-        <Route path="/write" element={currentUser ? <Write/> : <Login />} />{}
-        <Route path="/settings" element={currentUser ? <Settings /> : <Login />} />
+        <Route path="/write" element={currentUser ? <Write /> : <Login />} />
+        {}
+        <Route
+          path="/settings"
+          element={currentUser ? <Settings /> : <Login />}
+        />
       </Routes>
-    </Router>   
+    </Router>
   );
 }
 
--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -30,10 +30,8 @@ const Sidebar = () => {
         <span className="sidebarTitle">CATEGORIES</span>
         <ul className="sidebarList">
           {cats.map((c) => (
-            <Link to={`/?cat=${c.name}`} className="link">
-              <li className="sidebarListItem" key={c._id}>
-                {c.name}
-              </li>
+            <Link to={`/?cat=${c.name}`} className="link" key={c._id}>
+              <li className="sidebarListItem">{c.name}</li>
             </Link>
           ))}
         </ul>

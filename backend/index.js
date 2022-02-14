2d38f983192874df7ebccb6de62824396abb19dd
Pritam Debnath <49619791+Pritam-deb@users.noreply.github.com>
2022-02-14 21:15:29 +0530

    chore: fixed files for deploying the backend

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ dotenv.config();
 const __dirname = path.resolve();
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.MONGO_URL, () =>
   console.log("Conntected to mongoDB")
